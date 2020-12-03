553e0e2327d10d98e5379cfdeac3c003e063f2ee
Bryareos <david-o@jhiki.fr>
2020-12-03 11:27:14 +0100

    Test emoji findAll

--- src/main/java/emoji/App.java
+++ src/main/java/emoji/App.java
@@ -1,10 +1,7 @@
 package emoji;
 
-import emoji.Utils.DatabaseHandler;
 
-import java.io.FileNotFoundException;
-import java.sql.SQLException;
-import java.util.Scanner;
+import emoji.Model.Emoji;
 
 /**
  * Hello world!
@@ -18,6 +15,8 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!");
+        for (Emoji emoji: Emoji.findAll()) {
+            System.out.println(emoji.getCode() + " " +  emoji.getCharacters());
+        }
     }
 }

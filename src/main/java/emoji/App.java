d306fcdea78afc8c8b08fc6c726ae67166aaafd5
Bryareos <david-o@jhiki.fr>
2020-12-03 16:02:33 +0100

    First try in app

--- src/main/java/emoji/App.java
+++ src/main/java/emoji/App.java
@@ -2,6 +2,7 @@ package emoji;
 
 
 import emoji.Model.Emoji;
+import emoji.Utils.ConsoleColor;
 
 import java.util.Scanner;
 
@@ -16,13 +17,21 @@ public final class App {
      */
     public static void main(String[] args) throws Exception {
         for (Emoji emoji: Emoji.findAll()) {
-            System.out.println(emoji.getCode() + " " +  emoji.getCharacters());
+            System.out.println(emoji.getCode() + " - " +  emoji.getCharacters());
         }
         scanner = new Scanner(System.in);
         System.out.println("");
-        System.out.println("> ");
+        System.out.print("> ");
         String userInput = scanner.nextLine().trim().toLowerCase();
         Emoji emoji = Emoji.findByCode(userInput);
         System.out.println(emoji.getCharacters());
+
+        System.out.println(ConsoleColor.MAGENTA + "Rechercher un nom de catégorie et lister les emojis y appertenant " + ConsoleColor.RESET);
+        System.out.print("> ");
+        String input = scanner.nextLine().trim().toLowerCase();
+        System.out.println(ConsoleColor.MAGENTA + "Liste des emojis dans la catégorie `" + input + "`" + ConsoleColor.RESET);
+        for (Emoji emojiByCategory: Emoji.findByCategory(input)) {
+            System.out.println(emojiByCategory.getCode() + " " +  emojiByCategory.getCharacters());
+        }
     }
 }
